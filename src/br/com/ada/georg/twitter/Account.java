c67dd72e3b956a8eba566855f4da20204004e68a
georgrybski <rybskigeorg@gmail.com>
2022-12-15 18:23:27 -0300

    Integrated Array expanding functionality in the Account's follow method

--- src/br/com/ada/georg/twitter/Account.java
+++ src/br/com/ada/georg/twitter/Account.java
@@ -13,10 +13,10 @@ public class Account {
     private int tweetCount = 0;
 
     private Object[] followedList = new Object[1];
-    private int followerCount =0;
+    private int followCount = 0;
 
     private Object[] followerList = new Object[1];
-    private int followCount = 0;
+    private int followerCount =0;
 
     private Account(String username, String password, String creationDate ,User user) {
         this.username = username;
@@ -55,13 +55,24 @@ public class Account {
     }
 
     public static void follow(Account follower, Account followed) {
+        // Check if Already following
+
+        //Check if arrays are full, and expand them if necessary
+        var isFollowedListFull = followed.followerCount == followed.followerList.length-1;
+        var isFollowerListFull = follower.followCount == follower.followedList.length -1;
+
+        if (isFollowerListFull) {
+            follower.followedList = ArrayTools.returnExpandedArray(follower.followedList);
+        }
+        if (isFollowedListFull) {
+            followed.followerList = ArrayTools.returnExpandedArray(followed.followerList);
+        }
+
         // Add followed to follower's followed list
         follower.followedList[follower.followCount++] = followed;
         // Add follower to followed's follower list
         followed.followerList[followed.followerCount++] = follower;
     }
-
-
 
     public static Object[] getAccountList() {
         return accountList;
