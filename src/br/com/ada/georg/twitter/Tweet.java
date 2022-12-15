e11b91145e613a7ec2f39f9f2b6e36d7ea77c40e
georgrybski <rybskigeorg@gmail.com>
2022-12-15 15:07:15 -0300

    Implemented partial function for class Tweet

--- src/br/com/ada/georg/twitter/Tweet.java
+++ src/br/com/ada/georg/twitter/Tweet.java
@@ -1,4 +1,64 @@
 package br.com.ada.georg.twitter;
 
 public class Tweet {
+    private static Object[] tweetList = new Object[1];
+    private static int tweetCount = 0;
+
+    private String tweet;
+//    TODO add proper date structure
+    private String postDate;
+    private Account author;
+
+    private Object[] comments = new Object[1];
+    private int commentCount = 0;
+
+    private Object[] likes = new Object[1];
+    private int likeCount;
+
+    public static Object[] getTweetList() {
+        return tweetList;
+    }
+
+    //    TODO
+    public static void displayTweets(Object[] tweets) {
+
+
+    }
+
+    //    TODO
+    public static void displayTweet(Tweet tweet) {
+
+    }
+
+    public static int getTweetCount() {
+        return tweetCount;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
+
+    public String getPostDate() {
+        return postDate;
+    }
+
+    public Account getAuthor() {
+        return author;
+    }
+
+    public Object[] getComments() {
+        return comments;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public Object[] getLikes() {
+        return likes;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
 }
