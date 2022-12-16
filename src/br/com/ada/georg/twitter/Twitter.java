10f1fb924527a3b57418a311721c6d2be09af59a
georgrybski <rybskigeorg@gmail.com>
2022-12-16 18:03:19 -0300

    Added a experimental tweet frame printer that will later be implemented in printFormattedTweet

--- src/br/com/ada/georg/twitter/Twitter.java
+++ src/br/com/ada/georg/twitter/Twitter.java
@@ -2,8 +2,23 @@ package br.com.ada.georg.twitter;
 
 public class Twitter {
 
+    //TODO
     private static void printFormattedTweet(Tweet tweet) {
 
+
+    }
+
+    // Experimental
+    private static void printTweetFrame(int length, int height) {
+        printFrameLine(length, "+", "-");
+        for (int i = 0; i < height-2 ; i++) {
+            printFrameLine(length, "|", " ");
+        }
+        printFrameLine(length, "+", "-");
+    }
+
+    private static void printFrameLine(int length, String delimiter, String filler) {
+        System.out.println(delimiter + filler.repeat(length-2) + delimiter);
     }
 
     private static void printTweetsInList(Object[] tweetList) {
@@ -18,11 +33,19 @@ public class Twitter {
     }
 
 //    private static void pr
-    private static void viewProfile() {
+    private static void viewProfile(Account account) {
 
     }
 
     private static void viewTimelines() {
+
+    }
+
+    private static void viewFollowers() {
+
+    }
+
+    private static void viewFollowed() {
 
     }
 
